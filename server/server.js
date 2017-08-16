6614e894398449b62049b53a76fcd7f4a422ae7d
Owen Thomas <opthomas@xtra.co.nz>
2017-08-16 13:14:56 +1200

    Add GET /todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectId} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -25,6 +26,28 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+})
+
+// GET /todos/:id fetch todo by id
+app.get('/todos/:id', (req, res) => {
+
+    //Validate Id using isValid
+    var todoID = req.params.id;
+    
+    if(!ObjectId.isValid(todoID)){
+        res.status(404).send({Message:'ID is not valid', Error: 404});
+    }
+
+    Todo.findById(todoID).then((todo) => {
+        
+        if(!todo){
+            res.status(404).send('Todo not found');
+        }
+    
+        res.send({todo});
     }, (e) => {
         res.status(400).send(e);
     });
