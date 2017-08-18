65001f1e2cf7ccdb0831f0140210919e480a752f
Owen Thomas <opthomas@xtra.co.nz>
2017-08-18 15:29:56 +1200

    Added environment selection code

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+env = process.env.NODE_ENV || 'development';
+
+// mongoose.connect(process.env.DEV_MONGODB || 'mongodb://localhost:27017/TodoApp');
+
+if(env == 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
+console.log("***** env ::", env)
