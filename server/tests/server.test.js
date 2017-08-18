c8e17f41c368dc715f93b5444476cc42dc38616f
Owen Thomas <opthomas@xtra.co.nz>
2017-08-18 12:22:29 +1200

    Test delete todo by id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -107,3 +107,45 @@ describe('GET /todos/id', () => {
         
     })
 })
+
+describe('DELETE /todos/id', () => {
+    it('Should remove a todo', (done) => {
+
+        var hexId = todos[0]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId);
+        })
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist();
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+
+    it('Should return 404 if todo not found', (done) =>{
+        request(app)
+        .delete('/todos/' + (new ObjectId()).toHexString())
+        .expect(404)
+        .end(done);
+
+    })
+
+    it('Should return 404 for non object ids', (done) => {
+        //todos/123
+        request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done);
+        
+    })
+
+})
