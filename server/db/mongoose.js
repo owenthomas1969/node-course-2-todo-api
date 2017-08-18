c79b7f87b9080ee14a8a2a2ad7cfd913113f43db
Owen Thomas <opthomas@xtra.co.nz>
2017-08-18 15:40:27 +1200

    testing

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
-
+console.log("heroku env = ", env);
 module.exports = {mongoose};
 
